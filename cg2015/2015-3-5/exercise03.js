92c855ffc2aa99c8c1c312e2f0ee9632ed52faeb
Giulia <giulia@giulia-laptop.(none)>
2015-03-05 22:00:43 +0100

    esercizi 5 Marzo

---
+++ cg2015/2015-3-5/exercise03.js
@@ -0,0 +1,16 @@
+// write a script that prints the identity matrix
+
+var i,j;
+var result="";
+var value=0;
+for (i=1;i<=10;i++){
+	for (j=1;j<10;j++){
+		value=(i===j)?1:0;	
+		result+=value;
+		result+=",\t";
+		}
+	value=(i===j)?1:0;	
+	result+=value;
+	result+="\n";
+	}
+window.alert(result);
